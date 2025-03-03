8ee071bcd114812c414c33c89414a39453d35eb1
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-04 00:26:24 +0530

    First WebSocket Connection

--- WebSockets/First/dist/index.js
+++ WebSockets/First/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+//event handler
+wss.on("connection", function (socket) {
+    console.log("User Connected");
+    setInterval(() => {
+        socket.send("HEllo " + Math.round(100 * (Math.random())));
+    }, 2000);
+});
--- WebSockets/First/src/index.ts
+++ WebSockets/First/src/index.ts
@@ -1,3 +1,11 @@
 import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 });
+
+//event handler
+wss.on("connection", function(socket){
+    console.log("User Connected");
+    setInterval(() => {
+        socket.send("HEllo " + Math.round(100 * (Math.random())))
+    }, 2000);
+});
