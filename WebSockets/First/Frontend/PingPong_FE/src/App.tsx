92f3eb81e65edd10997eb419990e91bfd5ced813
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-05 22:26:55 +0530

    Restart The Frontend

--- WebSockets/First/Frontend/PingPong_FE/src/App.tsx
+++ WebSockets/First/Frontend/PingPong_FE/src/App.tsx
@@ -2,26 +2,10 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [socket, setSocket] = useState();
-  const inputRef = useRef();
 
-  function sendMessage() {
-    if (!socket) {
-      return;
-    }
-    const message = inputRef.current.value;
-    //@ts-ignore
-    socket.send(message)
+  function sendMessage(){
+    
   }
-
-  useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
-    setSocket(ws)
-
-    ws.onmessage = (ev) => {
-      alert(ev.data);
-    }
-  }, []);
 
   return (
     <div>
