20bb28925953a00357ed315f8a86c75c43c14222
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-16 13:44:05 +0530

    API

---
+++ Gemini/API/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="bg-black h-20 w-20">
+        ksdbvoevnaendvndaenv
+      </div>
+    </div>
+  )
+}
+
+export default App
