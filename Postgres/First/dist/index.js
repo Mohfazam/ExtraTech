6ae9f3a2553b374cb604f73e5e5341f841c6510d
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-17 01:26:18 +0530

    First Query

---
+++ Postgres/First/dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_RTzw5X1mglJV",
+//     port: 5432,
+//     host: "ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech",
+//     database: "neondb",
+//     ssl: true
+// });
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield pgClient.connect();
+        const response = yield pgClient.query("SELECT * FROM users;");
+        console.log(response.rows);
+    });
+}
+main();
--- Postgres/First/src/index.ts
+++ Postgres/First/src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello Nigga");
+import { Client, Query } from "pg";
+
+const pgClient = new Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_RTzw5X1mglJV",
+//     port: 5432,
+//     host: "ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech",
+//     database: "neondb",
+//     ssl: true
+// });
+
+async function main() {
+    await pgClient.connect();
+
+    const response = await pgClient.query("SELECT * FROM users;");
+    console.log(response.rows);
+}
+
+main()
