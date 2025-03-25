3952558166513b5fbcb8b8039cd65361863fea89
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-26 00:51:03 +0530

    First Self Connection 2

--- Postgres/OneShot/Begin/dist/index.js
+++ Postgres/OneShot/Begin/dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello World");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
+pgClient.connect();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield pgClient.query("CREATE DATABSE test3");
+        console.log(response.rows);
+    });
+}
+main();
--- Postgres/OneShot/Begin/src/index.ts
+++ Postgres/OneShot/Begin/src/index.ts
@@ -1,10 +1,14 @@
 import {Client, Query} from "pg";
 
 
-const pgClient = new Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
+const pgClient = new Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
 
 pgClient.connect();
 
 async function main(){
-    const response  = await pgClient.query()
+    const response  = await pgClient.query("CREATE DATABSE test3");
+
+    console.log(response.rows);
 }
+
+main();
