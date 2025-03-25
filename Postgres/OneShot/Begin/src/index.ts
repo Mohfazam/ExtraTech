9b240e6173a2d9f76b55a7603e75e3d1d0aa476c
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-26 00:41:10 +0530

    First Self Connection

--- Postgres/OneShot/Begin/src/index.ts
+++ Postgres/OneShot/Begin/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import {Client, Query} from "pg";
+
+
+const pgClient = new Client("postgresql://neondb_owner:npg_RTzw5X1mglJV@ep-calm-fire-a1l4vgj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
+
+pgClient.connect();
+
+async function main(){
+    const response  = await pgClient.query()
+}
