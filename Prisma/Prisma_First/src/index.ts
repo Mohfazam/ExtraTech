10c32e45ec3918a07d1b14f3304c48e6b7c28ef5
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-27 02:12:27 +0530

    Data Fetching and Updation using Prisma Generator

--- Prisma/Prisma_First/src/index.ts
+++ Prisma/Prisma_First/src/index.ts
@@ -1,1 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+
+const client  = new PrismaClient();
+
+async function createUser() {
+    await client.user.update({
+        where:{
+            id: 1
+        },
+        data :{
+            username: "Mohfazam"
+        }
+
+    });
+}
+
+
+async function FindUser() {
+    const user = await client.user.findFirst({
+        where:{
+            id: 1
+        }
+
+    });
+
+    console.log(user);
+}
+
+FindUser();
