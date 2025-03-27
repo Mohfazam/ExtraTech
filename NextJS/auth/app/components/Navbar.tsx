483d1e4c6842739d9988618f10b49ba7b050e465
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-28 01:23:53 +0530

    Layouts

---
+++ NextJS/auth/app/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export function Navbar(){
+    return(
+        <div className="border-b p-4 m-2">Navbar</div>
+    )
+}
---
+++ NextJS/auth/app/userauth/layout.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/Navbar";
+//@ts-ignore
+export default function AuthLayout({children}){
+    return(
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    )
+}
