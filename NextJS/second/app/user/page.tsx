e127d6d661a0cdca8d9bd3b0832cd4f0e731b999
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-28 16:30:47 +0530

    CSR

--- NextJS/second/app/user/page.tsx
+++ NextJS/second/app/user/page.tsx
@@ -1,5 +1,31 @@
+"use client"
+
+import axios from "axios";
+import {useState, useEffect} from "react";
+
 export default function user(){
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get(" https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details").then(response => {
+            setData(response.data);
+            setLoading(false);
+        })
+    }, []);
+
+    if(loading){
+        return <div>
+            Loading.........
+        </div>
+    }
+
     return <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Yo its an page route!!!!!!!!!!
+        <br /><br />
+        {data.name}
+        <br /><br />
+        {data.email}
     </div>
 }
