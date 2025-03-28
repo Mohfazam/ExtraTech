9fb152c56547456db9c9796baee80ca3f221ca02
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-28 16:50:42 +0530

    Loader in Next

--- NextJS/second/app/user/page.tsx
+++ NextJS/second/app/user/page.tsx
@@ -5,6 +5,9 @@ export default async function user(){
 
 
     const response = await axios.get(" https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+
+    await new Promise(r => setTimeout(r, 5000));
+
     const data = response.data;
 
     return <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
