2dbb15685fde39a223a158c96519beb60d9ff4fc
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-29 12:58:48 +0530

    Next Self Auth backend

--- NextJS/next_todo/app/signup/page.tsx
+++ NextJS/next_todo/app/signup/page.tsx
@@ -1,14 +1,28 @@
 "use client"
 
 import axios from "axios"
+import { useState } from "react";
 
 export default function signup(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
     return <div className="w-screen h-screen flex justify-center items-center">
         <div className="border p-2">
-            <input type="text" placeholder="username" /> 
-            <input type="password" placeholder="password" />
+            <input type="text" placeholder="username" onChange={(e) => {
+                setUsername(e.target.value);
+            }}/> 
+            <input type="password" placeholder="password" onChange={(e) => {
+                setPassword(e.target.value);
+            }}/>
             <button onClick={() => {
-                axios.post("http://localhost:3000/api/v1/signup")
+                axios.post("http://localhost:3000/api/v1/signup",{
+                    username,
+                    password
+                }
+                );
             }}>Sign Up</button>
         </div>
     </div>
