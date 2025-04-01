247e51785fb0092777a178109e17388281ff8c6d
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-02 00:48:02 +0530

    Next Auth for Google and Github

--- NextJS/next_auth/app/api/auth/[...nextauth]/route.ts
+++ NextJS/next_auth/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,24 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 import { NextResponse } from "next/server";
 
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
-          name: "Login With Email",
+          name: "Email",
           credentials: {
             username: { label: "Username", type: "text", placeholder: "username" },
-            password: { label: "Password", type: "password" }
+            password: { label: "Password", type: "password", placeholder:"password" }
           },
           async authorize(credentials, req) {
             const username = credentials?.username;
             const password = credentials?.password;
+
+            console.log(username);
+            console.log(password);
       
             const user = {
                 name: "sarwar",
@@ -27,7 +32,16 @@ const handler = NextAuth({
                 return null;
             }
           }
-        })
+        }),
+        GoogleProvider({
+            clientId: "process.env.GOOGLE_CLIENT_ID",
+            clientSecret: "process.env.GOOGLE_CLIENT_SECRET"
+            
+          }),
+          GitHubProvider({
+            clientId: "process.env.GITHUB_ID",
+            clientSecret: "process.env.GITHUB_SECRET"
+          })
       ]
 })
 
