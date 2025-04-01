309606926819d1b75d65d2cd6787d5ae11e517c0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-02 00:30:06 +0530

    Next Auth

--- NextJS/next_auth/app/api/auth/[...nextauth]/route.ts
+++ NextJS/next_auth/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,34 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-// const handler = NextAuth({
+import { NextResponse } from "next/server";
 
-// })
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+          name: "Login With Email",
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "username" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            const username = credentials?.username;
+            const password = credentials?.password;
+      
+            const user = {
+                name: "sarwar",
+                id: "1",
+                username: "sarwar@email.com"
+            }
 
-// export {handler as GET, handler as POST}
+            if(user){
+                return user;
+            } else{
+                return null;
+            }
+          }
+        })
+      ]
+})
+
+export {handler as GET, handler as POST}
