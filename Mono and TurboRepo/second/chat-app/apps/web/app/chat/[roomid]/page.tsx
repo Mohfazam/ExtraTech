0f421f9e92a5630d18da924eacc1b1582eff4d66
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-14 01:44:59 +0530

    awjdbv

--- Mono and TurboRepo/second/chat-app/apps/web/app/chat/[roomid]/page.tsx
+++ Mono and TurboRepo/second/chat-app/apps/web/app/chat/[roomid]/page.tsx
@@ -1,7 +1,21 @@
+import { TextInput } from "@repo/ui/Text-input";
+
 export default function ChatRoom() {
     return(
+        <div style={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent:"space-between",
+            flexDirection:"column"
+        }}>
         <div>
         Chatroom
+        </div>
+
+        <div>
+            <TextInput placeholder="Enter your Msg" size="big"/>
+        </div>
         </div>
     )
 }
