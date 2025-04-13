c36cf0973d3d08a28e28bdf09a888d53d7cbdfbe
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-13 15:39:10 +0530

    UI Component

--- Mono and TurboRepo/second/chat-app/apps/web/app/page.tsx
+++ Mono and TurboRepo/second/chat-app/apps/web/app/page.tsx
@@ -1,7 +1,11 @@
+import {TextInput} from '../../../packages/ui/src/Text-input'
+
 export default function Home(){
   return(
     <div>
+
       Hello
+      <TextInput />
     </div>
   )
 }
---
+++ Mono and TurboRepo/second/chat-app/packages/ui/src/Text-input.tsx
@@ -0,0 +1,19 @@
+interface PropType{
+    placeholder: string;
+}
+
+export function TextInput({
+    placeholder,
+}: PropType){
+    return(
+        <div>
+            <input type="text" placeholder={placeholder} style={{
+                padding: 10,
+                margin: 10,
+                borderColor: "black",
+                borderWidth: 1,
+                
+            }}></input> 
+        </div>
+    )
+}
