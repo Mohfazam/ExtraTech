c03af9d421f022565f16e02bcc0293c9c545df46
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-13 16:46:58 +0530

    TextInput Component 2

--- Mono and TurboRepo/second/chat-app/apps/web/app/page.tsx
+++ Mono and TurboRepo/second/chat-app/apps/web/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home(){
     }}>
 
       Hello
-      <TextInput placeholder='World'/>
+      <TextInput placeholder='World' size='big'/>
     </div>
   )
 }
--- Mono and TurboRepo/second/chat-app/packages/ui/src/Text-input.tsx
+++ Mono and TurboRepo/second/chat-app/packages/ui/src/Text-input.tsx
@@ -1,15 +1,17 @@
 interface PropType{
     placeholder: string;
+    size: "big" | "small";
 }
 
 export function TextInput({
     placeholder,
+    size
 }: PropType){
     return(
         <div>
             <input type="text" placeholder={placeholder} style={{
-                padding: 10,
-                margin: 10,
+                padding: size === "big" ? 30 : 10,
+                margin: size === "big" ? 30 : 10,
                 borderColor: "black",
                 borderWidth: 1,
                 
